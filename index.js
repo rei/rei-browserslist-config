4ea2f5f58f64d6f4b8e4f1ea9572ef0b7ab769a8
Alex Perkins <aperkin@rei.com>
2022-01-05 12:20:19 -0800

    Updating versions
    - > 0.3% visits from 11/01/2021 - 01/01/2022.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = [
-  'Chrome >= 79',
-  'Firefox >= 85',
+  'Chrome >= 78',
+  'Firefox >= 93',
   'iOS > 12',
+  'ios_saf >= 12',
   'Safari >= 12',
-  'Edge >= 88',
+  'Edge >= 95',
 ]; 
